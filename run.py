fc63219a7efc68d764b33c1eb900897ef592ccf4
PjHurtig <>
2023-05-29 16:46:52 +0000

    add validation function to check for integer compatability of input data

--- run.py
+++ run.py
@@ -32,7 +32,9 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there are not exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values requred, you provided {len(values)}"
@@ -41,3 +43,4 @@ def validate_data(values):
         print(f"invalid data: {e}, please try again.\n")
 
 get_sales_data()
+
